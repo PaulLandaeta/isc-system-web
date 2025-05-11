110e47dcae5417ea3a7b92783bba915094ab3a7a
LeonardoVNC <LeonardoVNC@users.noreply.github.com>
2025-05-11 18:37:56 -0400

    feat(dynamic-menu): [520] adapted permission helper to new structure

--- src/helper/permissions.ts
+++ src/helper/permissions.ts
@@ -1,18 +1,7 @@
-import { Permission } from '../models/permissionInterface';
-import { useUserStore } from '../store/store';
+import { useUserDataStore } from '../store/store';
 
 export function HasPermission(permissionName: string): boolean {
-  const user = useUserStore((state) => state.user);
-  let hasThePermission = false;
-  if (user) {
-    Object.keys(user.roles_permissions).forEach((role_number: string) => {
-      const rolePermissions = user.roles_permissions[role_number];
-      rolePermissions?.permissions.forEach((permission: Permission) => {
-        if (permissionName.toLowerCase() === permission.name.toLowerCase()) {
-          hasThePermission = true;
-        }
-      });
-    });
-  }
-  return hasThePermission;
+  const permission = permissionName.toLowerCase();
+  const userPermissions = useUserDataStore((state) => state.permissions);
+  return userPermissions.length > 0 && userPermissions.includes(permission);
 }
